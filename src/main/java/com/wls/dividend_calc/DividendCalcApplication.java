aeb2ca64fd234a213d63158fe5b82a78e423e5b5
jason white <jason_white_01@hotmail.com>
2024-09-05 16:21:24 -0600

    first commit

---
+++ src/main/java/com/wls/dividend_calc/DividendCalcApplication.java
@@ -0,0 +1,13 @@
+package com.wls.dividend_calc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DividendCalcApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DividendCalcApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/wls/dividend_calc/DividendCalcApplicationTests.java
@@ -0,0 +1,13 @@
+package com.wls.dividend_calc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DividendCalcApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
