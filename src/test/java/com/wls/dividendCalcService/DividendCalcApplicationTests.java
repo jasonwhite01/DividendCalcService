3932c92e8628f54a347f5d22c3222695764f66aa
jason white <jason_white_01@hotmail.com>
2024-09-06 17:05:40 -0600

    added super-basic test

--- src/test/java/com/wls/dividendCalcService/DividendCalcApplicationTests.java
+++ src/test/java/com/wls/dividendCalcService/DividendCalcApplicationTests.java
@@ -1,13 +1,23 @@
 package com.wls.dividendcalcservice;
 
 import org.junit.jupiter.api.Test;
+import static org.assertj.core.api.Assertions.assertThat;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+
+import com.wls.dividendcalcservice.controller.DividendController;
 
 @SpringBootTest
 class DividendCalcApplicationTests {
 
+	//TODO: add tests
+
+	@Autowired
+	private DividendController controller;
+
 	@Test
-	void contextLoads() {
+	void contextLoads() throws Exception {
+		assertThat(controller).isNotNull();
 	}
 
 }
